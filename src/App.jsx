756eb880a8379869825ea197ab3fcc07f2ef1838
4lPeReZ <alvaropr05@hotmail.com>
2022-12-26 15:26:06 +0100

    Tips

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const GIPHY_API_KEY = "HqSU9kMh3q0kwP93m5bhl5Kfaqd05d48"; // need a new Key
+
 function App() {
 
-  const GIPHY_API_KEY = "HqSU9kMh3q0kwP93m5bhl5Kfaqd05d48"; // need a new Key
   const [catFact, setCatFact] = useState('');
   const [catGif, setCatGift] = useState('');
 
@@ -15,22 +16,18 @@ function App() {
       //console.log(data.data[0].images.original.url)
       setCatGift(data.data[0].images.original.url)
     });
-
   };
-
 
   const callAPI = () => {
     fetch('https://catfact.ninja/fact')
     .then((res) => res.json())
     .then((data) => {
       setCatFact(data.fact || "La API no funciona");
-      callGiphyAPI(data.fact.split(' ').slice(0, 3).join(' '));
+      callGiphyAPI(data.fact.split(' ').slice(0, 3).join(' ')); // Si se pone .split(' ',3).join(' ') haria lo mismo
       console.log(data.fact);
     });
     //console.log("API Call");
   };
-
-
 
   useEffect(
     callAPI,

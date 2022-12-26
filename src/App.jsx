e07f95a5f1166c5ece0662ec10f57a8134f11c07
4lPeReZ <alvaropr05@hotmail.com>
2022-12-26 13:39:04 +0100

    Call API + Show the fact

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [catFact, setCatFact] = useState('')
+
+  const callAPI = () => {
+    fetch('https://catfact.ninja/fact')
+    .then((res) => res.json())
+    .then((data) => {
+      setCatFact(data.fact);
+      console.log(data.fact);
+    });
+    //console.log("API Call");
+  };
+
+  
+
+  useEffect(
+    callAPI,
+    [],
+  );
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+
+
+      <h1>{catFact}</h1>
+
+
     </div>
   )
 }

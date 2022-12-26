9626072689b20d6f410e3cbf708e5ee3f4aea1c6
4lPeReZ <alvaropr05@hotmail.com>
2022-12-26 14:57:39 +0100

    Get a Gif with the 3 first words from the Fact

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,33 @@ import './App.css'
 
 function App() {
 
-  const [catFact, setCatFact] = useState('')
+  const GIPHY_API_KEY = "HqSU9kMh3q0kwP93m5bhl5Kfaqd05d48"; // need a new Key
+  const [catFact, setCatFact] = useState('');
+  const [catGif, setCatGift] = useState('');
+
+  const callGiphyAPI = (string) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${string}&api_key=${GIPHY_API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      //console.log(data.data[0].images.original.url)
+      setCatGift(data.data[0].images.original.url)
+    });
+
+  };
+
 
   const callAPI = () => {
     fetch('https://catfact.ninja/fact')
     .then((res) => res.json())
     .then((data) => {
-      setCatFact(data.fact);
+      setCatFact(data.fact || "La API no funciona");
+      callGiphyAPI(data.fact.split(' ').slice(0, 3).join(' '));
       console.log(data.fact);
     });
     //console.log("API Call");
   };
 
-  
+
 
   useEffect(
     callAPI,
@@ -28,6 +42,7 @@ function App() {
 
 
       <h1>{catFact}</h1>
+      <img src={catGif}/>
 
 
     </div>
